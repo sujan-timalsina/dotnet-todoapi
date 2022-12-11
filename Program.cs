4f6b9c12843ed7df1ea83d37d738017e379c263e
sujan-timalsina <sujan.timalsina@apexcollege.edu.np>
2022-12-11 08:49:54 +0545

    added package sqlite

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
+builder.Services.AddDbContext<TodoContext>(opt => opt.UseSqlite("Data Source=TodoApi.sqlite3"));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
